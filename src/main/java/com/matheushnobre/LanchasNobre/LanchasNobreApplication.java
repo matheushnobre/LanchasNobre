04fd1d6985cedd304091018ca2be34aa1f75fd0b
Matheus <matheus1204h@gmail.com>
2025-07-15 17:49:53 -0300

    first commit

---
+++ src/main/java/com/matheushnobre/LanchasNobre/LanchasNobreApplication.java
@@ -0,0 +1,13 @@
+package com.matheushnobre.LanchasNobre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchasNobreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchasNobreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheushnobre/LanchasNobre/LanchasNobreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheushnobre.LanchasNobre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchasNobreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
